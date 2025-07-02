9e5de69441721920cd1844690ab068a89e9b592f
gareng147 <dendipp624@gmail.com>
2025-07-02 19:28:46 +0700

    up PS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.schedu"
+    namespace = "com.schedu.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -40,7 +40,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.schedu"
+        applicationId = "com.schedu.app"
         
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
--- android/app/src/main/kotlin/com/schedu/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/schedu/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.schedu
+package com.schedu.app
 
 import io.flutter.embedding.android.FlutterActivity
 
